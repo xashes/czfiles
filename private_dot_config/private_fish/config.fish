1b59af6a530496113b38484bd56e1f1be4f74269
xashes <xashes@outlook.com>
2021-01-24 20:56:45 +0800

    alias

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,3 +3,11 @@ set -x PATH ~/bin $PATH
 set -x PATH ~/.local/bin $PATH
 
 set -x EDITOR vim
+
+alias cht cht.sh
+alias cz chezmoi
+
+# git alias
+alias gst "git status"
+alias ga "git add"
+alias gcm "git commit -m"
