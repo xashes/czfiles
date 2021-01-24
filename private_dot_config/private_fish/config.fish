5533be340235c5a3ad6e90fce148f4e8685bb991
xashes <xashes@outlook.com>
2021-01-24 18:58:41 +0800

    python scripts

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,2 +1,5 @@
 set -x PATH ~/.emacs.d/bin $PATH
 set -x PATH ~/bin $PATH
+set -x PATH ~/.local/bin $PATH
+
+set -x EDITOR vim
---
+++ run_once_python-scripts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# python ./scripts/doom_install.py
+python scripts/python_env.py
+# python ./scripts/get_repos.py
