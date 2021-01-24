f34038ce2be14127ce2515bdccaef7dc452d9c3a
xashes <xashes@outlook.com>
2021-01-24 21:35:49 +0800

    fix path

--- run_once_python-scripts.sh
+++ run_once_python-scripts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# python ./scripts/doom_install.py
-python ./scripts/get_repos.py
-python scripts/install_cheat.py
+# python ~/scripts/doom_install.py
+python ~/scripts/get_repos.py
+python ~/scripts/install_cheat.py
