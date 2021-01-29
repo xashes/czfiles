d201633244e7e4c867d495db253efe2e4d5c5097
xashes <xashes@outlook.com>
2021-01-29 19:51:34 +0800

    fcitx

--- run_once_python-env.sh
+++ run_once_python-env.sh
@@ -4,10 +4,13 @@ sudo pacman -S python-pip --needed
 sudo pacman -S nodejs --needed
 sudo pacman -S npm --needed
 
+pip install -U wheel
 pip install -U pytest
 pip install -U black
 pip install -U pyflakes
 pip install -U isort
 pip install -U paramiko
+pip install -U pandas
+pip install -U jupyterlab
 
-sudo npm install -g pyright
+sudo npm update -g pyright
